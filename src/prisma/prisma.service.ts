e4dd203457e7b82b75427730337e9b6386ece92d
samxietyy <samussele@gmail.com>
2026-01-28 13:30:20 +0100

    Prisma fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 
 @Injectable()
